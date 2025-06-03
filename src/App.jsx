b9360f5c927afae0faf81d684018d36132add686
binda rama <crakun95@gmail.com>
2025-06-03 16:24:51 +0800

    first

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./index.css";
+
+function App() {
+  return (
+    <div>
+      <div className="text-blue-500 flex items-center justify-center font-bold">RRTM by JOKI UBG</div>
+    </div>
+  );
+}
+
+export default App;
